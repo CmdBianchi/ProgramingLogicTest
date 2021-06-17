844943f36a5d3ff5d282f59478229243313b2ba7
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-17 11:25:52 -0300

    Adicionar arquivos de projeto.

---
+++ ProgramingLogicTest/Entities/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Entities {
+    class Calculator {
+        public double PieceX { get; set; }
+        public int QuantityX { get; set; }
+        public double PieceY { get; set; }
+        public int QuantityY { get; set; }
+        public Calculator(double pieceX, int quantityX, double pieceY, int quantityY) {
+            PieceX = pieceX;
+            QuantityX = quantityX;
+            PieceY = pieceY;
+            QuantityY = quantityY;
+        }
+        public double TotalAccount() {
+            return PieceX * QuantityX + PieceY * QuantityY;
+        }
+        public override string ToString() {
+            return "VALOR A PAGAR: R$" + TotalAccount();
+        }
+    }
+}
---
+++ ProgramingLogicTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Entities;
+namespace ProgramingLogicTest {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre com o VALOR do PRODUTO 1 e QUANTIDADE: ");
+            double priceX = double.Parse(Console.ReadLine());
+            int quantityX = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nEntre com o VALOR do PRODUTO 2 e QUANTIDADE: ");
+            double priceY = double.Parse(Console.ReadLine());
+            int quantityY = int.Parse(Console.ReadLine());
+            Calculator c = new Calculator(priceX, quantityX, priceY, quantityY);
+            Console.WriteLine(c);
+        }
+    }
+}
