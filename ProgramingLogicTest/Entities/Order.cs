b9a052ff51ca3ea2e421be5b280ef054a5d2d914
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-17 21:05:46 -0300

    Commit 3 - Question 2.1

---
+++ ProgramingLogicTest/Entities/Order.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Entities {
+    class Order {
+        public int OrderId { get; set; }
+        public double Amount { get; set; }
+        public Order() {
+        }
+        public Order(int orderId, double amount) {
+            OrderId = orderId;
+            Amount = amount;
+        }
+        public double Total() {
+            if (OrderId == 1) {                
+                return Amount * 4.0;
+                
+            }
+            else if (OrderId == 2 ){
+                return Amount * 4.5;
+                
+            }
+            else if (OrderId == 3) {
+                return Amount * 5.0;
+                
+            }
+            else if (OrderId == 4) {
+                return Amount * 2.0;               
+            }
+            else {
+                return Amount * 1.5;           
+            }            
+        }
+        public override string ToString() {
+            return "Valor total a pagar: $" + Total();
+        }
+    }
+}
--- ProgramingLogicTest/Program.cs
+++ ProgramingLogicTest/Program.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using Entities;
+using System.Collections.Generic;
+using System.Linq;
 namespace ProgramingLogicTest {
     class Program {
         static void Main(string[] args) {
-            Console.Write("Entre com o VALOR DO RAIO: ");     
-            double radius = double.Parse(Console.ReadLine());
+            List<Menu> list = new List<Menu>() {
+                new Menu(){Id = 1, Name = "HotDog", Price = 4.00},
+                new Menu(){Id = 2, Name = "X-Salada", Price = 4.50},
+                new Menu(){Id = 3, Name = "X-Bancon", Price = 5.00},
+                new Menu(){Id = 4, Name = "Torrada", Price = 2.00},
+                new Menu(){Id = 5, Name = "CocaCola", Price = 1.50}                            
+            };
+            foreach(Menu obj in list) {
+                Console.WriteLine("Id " + obj.Id + " | " + obj.Name + ", R$ " + obj.Price);
+            }
             Console.WriteLine();
-            Calculator c = new Calculator(radius);                     
-            Console.WriteLine(c);
+            Console.WriteLine("-------------------------------------------");
+            Console.Write("Digite o Id do produto: "); int orderId = int.Parse(Console.ReadLine());
+            Console.Write("Digite a quantidade do produto: "); double amount = double.Parse(Console.ReadLine());          
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine();
+            Order o = new Order(orderId, amount);
+            Console.WriteLine(o);
         }
     }
 }
