4b012b40b586f6d4195ea72fc107bd729811888a
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-18 08:24:01 -0300

    Commit 2.1.1

--- ProgramingLogicTest/Entities/Order.cs
+++ ProgramingLogicTest/Entities/Order.cs
@@ -5,24 +5,19 @@ namespace Entities {
     class Order {
         public int OrderId { get; set; }
         public double Amount { get; set; }
-        public Order() {
-        }
         public Order(int orderId, double amount) {
             OrderId = orderId;
             Amount = amount;
         }
         public double Total() {
             if (OrderId == 1) {                
-                return Amount * 4.0;
-                
+                return Amount * 4.0;               
             }
             else if (OrderId == 2 ){
                 return Amount * 4.5;
-                
             }
             else if (OrderId == 3) {
-                return Amount * 5.0;
-                
+                return Amount * 5.0;  
             }
             else if (OrderId == 4) {
                 return Amount * 2.0;               
